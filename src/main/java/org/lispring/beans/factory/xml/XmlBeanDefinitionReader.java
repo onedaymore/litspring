18b8e5e31fcd6fae5bb1265efbc89ea3b5ee08d8
onedaymore <andayfour@gmail.com>
2018-08-19 02:02:01 +0800

    读取文件抽象为读取Reource，新增FileSystem..context

--- src/main/java/org/lispring/beans/factory/xml/XmlBeanDefinitionReader.java
+++ src/main/java/org/lispring/beans/factory/xml/XmlBeanDefinitionReader.java
@@ -13,6 +13,7 @@ import org.lispring.beans.factory.BeanDefinitionException;
 import org.lispring.beans.factory.BeanFactory;
 import org.lispring.beans.factory.support.BeanDefinitionRegistry;
 import org.lispring.beans.factory.support.GenericBeanDefinition;
+import org.lispring.core.io.Resource;
 import org.lispring.util.ClassUtils;
 
 /**
@@ -51,6 +52,40 @@ public class XmlBeanDefinitionReader {
 			}
 		} catch (DocumentException e) {
 			throw new BeanDefinitionException("BeanDefinitionException");
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+	
+	public void loadDefinitions(Resource resource) {
+		InputStream is = null;
+		SAXReader reader = new SAXReader();
+		try {
+			is = resource.getInputStream();
+			Document doc = reader.read(is);
+			
+			Element root = doc.getRootElement();//<beans>
+			Iterator<Element> it = root.elementIterator();
+			while (it.hasNext()) {
+				Element ele = it.next();
+				String beanId = ele.attributeValue(ID_ATTR);
+				String className = ele.attributeValue(CLASS_ATTR);
+				BeanDefinition bd = new GenericBeanDefinition(beanId, className);
+				this.registry.registryBeanDefinition(beanId, bd);
+			}
+		} catch (DocumentException e) {
+			throw new BeanDefinitionException("BeanDefinitionException");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} finally {
 			if (is != null) {
 				try {
